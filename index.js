cd23ae2158e1b23e61a1946eed1701bc089f077f
Fernando Nunes <95880342+DevFernandoNunes@users.noreply.github.com>
2022-03-30 10:18:24 -0300

    Update and create new files

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /* Constantes */
+require("dotenv").config();
 const express = require("express");
 const { required } = require("nodemon/lib/config");
 const app = express();
-const port = 3000;
+
 const path = require("path");
 
 /* Configuração do servidor */
@@ -10,70 +11,9 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
-/* Lógica do servidor */
-const pokedex = [
-  {
-    id: 1,
-    nome: "Bulbasaur",
-    descricao:
-      " There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.  ",
-    tipo: "Grass",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-  },
-  {
-    id: 2,
-    nome: "Charmander",
-    descricao:
-      "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail. ",
-    tipo: "Fire",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-  },
-  {
-    id: 3,
-    nome: "Squirtle ",
-    descricao:
-      "When it retracts its long neck into its shell, it squirts out water with vigorous force",
-    tipo: "Water",
-    imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-  },
-];
-
-let pokemon = undefined;
-
-/* Rotas */
-app.get("/", (req, res) => {
-  res.render("index", { pokedex, pokemon });
-});
-
-app.post("/create", (req, res) => {
-  const pokemon = req.body;
-  pokemon.id = pokedex.length + 1;
-  pokedex.push(pokemon);
-  res.redirect("/");
-});
-
-app.get("/detalhes/:id", (req, res) => {
-  const id = +req.params.id;
-  pokemon = pokedex.find((pokemon) => pokemon.id === id);
-  res.redirect("/");
-});
-
-app.post("/update/:id", (req, res) => {
-  const id = +req.params.id - 1;
-  const newPokemon = req.body;
-  newPokemon.id = id + 1;
-  pokedex[id] = newPokemon;
-  pokemon = undefined;
-  res.redirect("/");
-});
-
-app.get("/delete/:id", (req, res) => {
-  const id = +req.params.id - 1;
-  delete pokedex[id];
-  res.redirect("/");
-});
-
 /* Rodando servidor */
+const port = process.env.PORT || 3000;
+
 app.listen(port, () =>
   console.log("servidor rodando em http://localhost:3000/")
 );
